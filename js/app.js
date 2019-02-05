fab19466e89a5c99f27e0949245b0348e27965fc
hudsonst <hudsonst@users.noreply.github.com>
2019-02-04 20:20:46 -0500

    Initial version
    Initial version of Memory Card Game project.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 /*
  * Create a list that holds all of your cards
  */
-
-
+const allCards = Array.from(document.querySelectorAll('.card'));
+const deck = document.querySelector('.deck');
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -12,7 +12,8 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -25,6 +26,16 @@ function shuffle(array) {
     return array;
 }
 
+const shuffledCards = shuffle(allCards);
+const cardFragment = document.createDocumentFragment();
+
+for (let i = 0; shuffledCards.length > i; i++) {
+    console.log(shuffledCards[i]);
+    const newCard = shuffledCards[i];
+    cardFragment.appendChild(newCard);
+};
+
+deck.appendChild(cardFragment);
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -36,3 +47,105 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+let matchcounter = 0;
+let moves = 0;
+let lastCard = '';
+let time = 0;
+let starCount = 3;
+
+function respondToTheClick(event) {
+    const card = event.target;
+    showSymbol(card);
+    compareCards(card);
+    incrementMoves();
+    startTimer();
+}
+
+function showSymbol(card) {
+    card.classList.add('open', 'show');
+}
+
+function compareCards(card) {
+    let currentCard = cardArray[0];
+    if (currentCard === 'default') {
+        cardArray.pop();
+        cardArray.push(card);
+    } else if (currentCard === card) {
+        console.log("same card");
+        return;
+    } else if (currentCard.innerHTML === card.innerHTML) {
+        card.classList.add('match');
+        currentCard.classList.add('match');
+        matchcounter++;
+        countMatches();
+        cardArray.splice(0, 1, 'default');
+    } else if (currentCard.innerHTML !== card.innerHTML) {
+        setTimeout(function() {
+            currentCard.classList.remove('open', 'show');
+            card.classList.remove('open', 'show');
+        }, 1000);
+        cardArray.splice(0, 1, 'default');
+    }
+}
+
+
+function incrementMoves() {
+    moves++;
+    let moveNum = Array.from(document.querySelectorAll('.moves'));
+    moveNum.forEach(function(e) {
+        if (moves === 1) {
+            e.textContent = moves + ' move';
+        } else {
+            e.textContent = moves + ' moves';
+        }
+    })
+
+    const score = document.querySelector('.stars');
+    const star = score.firstElementChild;
+    if (moves >= 91) {
+        score.removeChild(star);
+        starCount = 0;
+    }
+    if (moves === 61) {
+        score.removeChild(star);
+        starCount = 1;
+    }
+    if (moves === 31) {
+        score.removeChild(star);
+        starCount = 2;
+    }
+
+}
+
+function countMatches() {
+    const modal = document.querySelector('.modal');
+    if (matchcounter === 8) {
+        stopTimer();
+        const final = document.querySelector('.modal .moves');
+        final.textContent = moves;
+        const finalTime = document.querySelector('#timer');
+        finalTime.textContent = time;
+        const starCountFinal = document.querySelector('#starCount');
+        if (starCount === 1) {
+            starCountFinal.textContent = starCount + ' star';
+        } else {
+            starCountFinal.textContent = starCount + ' stars';
+        }
+        modal.style.display = 'block';
+    }
+}
+
+function startTimer() {
+    time = setInterval(function() {
+        time++;
+    }, 1000);
+}
+
+function stopTimer() {
+    clearTimeout(time);
+}
+
+
+
+let cardArray = ['default'];
+deck.addEventListener('click', respondToTheClick);
